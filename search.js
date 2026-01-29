b8c2e575a11669d47cc9c195ed222bfcbc4c3947
TarasSych17 <98188750+TarasSych17@users.noreply.github.com>
2026-01-29 14:15:15 +0200

    Update search.js

--- search.js
+++ search.js
@@ -12,10 +12,7 @@ document.getElementById('phoneSearchForm').addEventListener('submit', function(e
     // Масив з посиланнями, в які підставляється номер телефону
     const urls = [
         `https://billing.columbus.te.ua/calendar.php?jbs_aux_name0=phone&jbs_aux_value0=${phone}&jcal_a=&jbs_id=5735650#jbs`,
-        `https://billing.columbus.te.ua/con_requests.php?crqs_phone_filter=${phone}#cnrper`,
-        `https://billing.columbus.te.ua/clients.php?cls_mode=default&cls_aux_name0=phones&cls_aux_value0=${phone}&cls_id=1861204&cls_page=1&cmenu_selected=jo#cls`,
-        `https://billing.columbus.te.ua/asterisk_calls.php?sacs2_src_name_filter=${phone}#sacs2`,
-        `https://billing.columbus.te.ua/asterisk_calls.php?sacs2_dst_filter=${phone}#sacs2`
+       
     ];
 
     // Відкриваємо всі посилання одночасно у нових вкладках
@@ -23,3 +20,4 @@ document.getElementById('phoneSearchForm').addEventListener('submit', function(e
         window.open(url, '_blank'); // '_blank' відкриває посилання в новій вкладці
     });
 });
+
