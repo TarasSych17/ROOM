5967306073b0f54b8080d0753545d2ace083812e
TarasSych17 <98188750+TarasSych17@users.noreply.github.com>
2026-03-15 11:03:32 +0200

    Refactor phone search form to open single link

--- search.js
+++ search.js
@@ -1,23 +1,32 @@
+// Перша форма
 document.getElementById('phoneSearchForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Не даємо формі виконувати звичайну поведінку (перезавантаження сторінки)
+    e.preventDefault();
 
     const phone = document.getElementById('phone').value.trim();
+
     if (!phone) {
         alert("Будь ласка, введіть номер телефону.");
-
-
         return;
     }
 
-    // Масив з посиланнями, в які підставляється номер телефону
-    const urls = [
-        `https://billing.columbus.te.ua/calendar.php?jbs_aux_name0=phone&jbs_aux_value0=${phone}&jcal_a=&jbs_id=5735650#jbs`,
-       
-    ];
+    const url = `https://billing.columbus.te.ua/calendar.php?jbs_aux_name0=phone&jbs_aux_value0=${phone}&jcal_a=&jbs_id=5735650#jbs`;
 
-    // Відкриваємо всі посилання одночасно у нових вкладках
-    urls.forEach(url => {
-        window.open(url, '_blank'); // '_blank' відкриває посилання в новій вкладці
-    });
+    window.open(url, '_blank');
 });
 
+
+// Друга форма
+document.getElementById('phoneSearchForm1').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const phone = document.getElementById('phone1').value.trim();
+
+    if (!phone) {
+        alert("Будь ласка, введіть номер заявки.");
+        return;
+    }
+
+    const url = `ТУТ_ДРУГЕ_ПОСИЛАННЯ${phone}`;
+
+    window.open(url, '_blank');
+});
