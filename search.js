4208981fef7b0e8c929a2c8b51a306fb6f2a7d9e
TarasSych17 <98188750+TarasSych17@users.noreply.github.com>
2026-08-28 14:45:46 +0300

    Update search.js

--- search.js
+++ search.js
@@ -47,6 +47,9 @@ document.getElementById('contractSearchForm').addEventListener('submit', functio
 
     window.open(url, '_blank');
 });
+
+
+// Пошук по прізвищу співробітника
 document.getElementById('staffSearchForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -58,6 +61,23 @@ document.getElementById('staffSearchForm').addEventListener('submit', function(e
     }
 
     const url = `https://billing.columbus.te.ua/staff.php?stf_name_alt_filter=${encodeURIComponent(staff)}#stf`;
+
+    window.open(url, '_blank');
+});
+
+
+// Пошук заявки по номеру телефону
+document.getElementById('conRequestSearchForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const phone = document.getElementById('conRequestPhone').value.trim();
+
+    if (!phone) {
+        alert("Будь ласка, введіть номер телефону.");
+        return;
+    }
+
+    const url = `https://billing.columbus.te.ua/con_requests.php?crqs_phone_filter=${encodeURIComponent(phone)}&crqs_aux_name0=phone&crqs_id=3236358&crqs_brief=0#crqs`;
 
     window.open(url, '_blank');
 });
